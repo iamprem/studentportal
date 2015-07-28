189e10a79d7fc0451cb16518d06fe82fef2c3d4e
ganesh <ganeshr89@yahoo.co.in>
2015-07-28 01:25:22 -0400

    Added Courses Classes

--- src/com/sp/model/Course.java
+++ src/com/sp/model/Course.java
@@ -5,14 +5,39 @@ public class Course {
 	private String courseName;
 	private String insMethod;
 	private int creditHours;
+	private String deptID;
+	private String degID;
 	
-	
+	public Course(int courseID, String courseName, String insMethod, int creditHours, String deptID, String degID) {
+		super();
+		this.courseID = courseID;
+		this.courseName = courseName;
+		this.insMethod = insMethod;
+		this.creditHours = creditHours;
+		this.deptID = deptID;
+		this.degID = degID;
+	}
 	public Course(int courseID, String courseName, String insMethod, int creditHours) {
 		super();
 		this.courseID = courseID;
 		this.courseName = courseName;
 		this.insMethod = insMethod;
 		this.creditHours = creditHours;
+	}
+	public String getDeptID() {
+		return deptID;
+	}
+	public void setDeptID(String deptID) {
+		this.deptID = deptID;
+	}
+	public String getDegID() {
+		return degID;
+	}
+	public void setDegID(String degID) {
+		this.degID = degID;
+	}
+	public void setInsMethod(String insMethod) {
+		this.insMethod = insMethod;
 	}
 	public int getCourseID() {
 		return courseID;
