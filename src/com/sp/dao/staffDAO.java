f3ba2868d7f096591fcc6f5c10e30fe7b643cfb0
Deepak Rohan <deepakrohan@hotmail.com>
2015-07-29 21:15:21 -0400

    DAO added for staff update

--- src/com/sp/dao/staffDAO.java
+++ src/com/sp/dao/staffDAO.java
@@ -23,15 +23,16 @@ public class staffDAO {
 		try {
 			conn = new DbConnection();
 			System.out.println("Fetching Staff all Application overview");
-			String sql1 = "SELECT S.employee_id,S.firstName,S.lastName,S.ssn,S.phone,S.streetAddress,S.apartmentNo,S.city,S.stateOrTeritory,S.country,S.zipcode,W.dept_id from staff S INNER JOIN staff_worksin_dept W on S.employee_id=W.employee_id WHERE S.email='"
+			String sql1 = "SELECT S.employee_id,S.firstName,S.lastName,S.email,S.ssn,S.phone,S.streetAddress,S.apartmentNo,S.city,S.stateOrTeritory,S.country,S.zipcode,W.dept_id from staff S INNER JOIN staff_worksin_dept W on S.employee_id=W.employee_id WHERE S.email='"
 					+ email + "';";
 			stmt = conn.DbConnectionForPreparedStatement(sql1);
 			ResultSet rs = stmt.executeQuery(sql1);
 			while (rs.next()) {
-				int app_id = rs.getInt("employee_id");
+				int employee_id = rs.getInt("employee_id");
 				String firstName = rs.getString("firstName");
 				String lastName = rs.getString("lastName");
-				String ssn = rs.getString("lastName");
+				String emailUser = rs.getString("email");
+				String ssn = rs.getString("ssn");
 				String phone = rs.getString("phone");
 				String streetAddress = rs.getString("streetAddress");
 				String apartmentNo = rs.getString("apartmentNo");
@@ -40,7 +41,7 @@ public class staffDAO {
 				String country = rs.getString("country");
 				String zipcode = rs.getString("zipcode");
 				String dept_id = rs.getString("dept_id");
-				 staff = new Staff(app_id, firstName, lastName, ssn, phone, streetAddress, apartmentNo, city,
+				 staff = new Staff(employee_id, firstName, lastName,emailUser, ssn, phone, streetAddress, apartmentNo, city,
 						stateOrTeritory, country, zipcode, dept_id);
 			}
 			rs.close();
